f081d71d1a5ce1a127da8beb6c9df4e6f475d5a1
Vu Dat <datpro2810@gmail.com>
2020-12-17 13:39:41 +0700

    Edit Out Brands

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => 'root',
 		'password' => '',
-		'database' => '16database',
+		'database' => '1stgear',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
--- app/Views/index.php
+++ app/Views/index.php
@@ -1919,37 +1919,47 @@
                         <div class="client_logo carousel_slider owl-carousel owl-theme nav_style3" data-dots="false" data-nav="true" data-margin="30" data-loop="true" data-autoplay="true" data-responsive='{"0":{"items": "2"}, "480":{"items": "3"}, "767":{"items": "4"}, "991":{"items": "5"}, "1199":{"items": "6"}}'>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo1.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_razer.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo2.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_logitech.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo3.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_edra.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo4.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_dareu.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo5.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_corsair.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo6.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_zowie.png" alt="cl_logo">
                                 </div>
                             </div>
                             <div class="item">
                                 <div class="cl_logo">
-                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo7.png" alt="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_asus.png" alt="cl_logo">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_samsung.png" alt="cl_logo">
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="cl_logo">
+                                    <img src="<?= base_url() ?>/public/client/assets/images/cl_logo_thinkpad.png" alt="cl_logo">
                                 </div>
                             </div>
                         </div>
